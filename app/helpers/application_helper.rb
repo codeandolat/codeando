2d66ed536e35015f1d8694943aa4af4b69c13c59
David Sanchez <sanchez.dav90@gmail.com>
2018-10-07 20:13:09 -0500

    Add flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # helper for all kind of flash messages
+  def flash_class(level)
+    case level.to_sym
+    when :notice then "notification is-info"
+    when :success then "notification is-success"
+    when :error then "notification is-danger"
+    when :alert then "notification is-warning"
+    end
+  end
 end

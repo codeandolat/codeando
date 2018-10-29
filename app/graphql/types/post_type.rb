559eaff6bb251e4888261c9de0686f53a33c969e
David Sanchez <sanchez.dav90@gmail.com>
2018-10-28 22:48:12 -0600

    Add post type graphql

---
+++ app/graphql/types/post_type.rb
@@ -0,0 +1,8 @@
+module Types
+  class PostType < Types::BaseObject
+    field :id, ID, null: false
+    field :title, String, null: false
+    field :subtitle, String, null: false
+    field :author, Types::UserType, null: false
+  end
+end

23c190918c8e81cc4dec42f0c8875a48c40c41d5
David Sanchez <sanchez.dav90@gmail.com>
2018-10-29 23:11:10 -0600

    Add first posts component

---
+++ app/javascript/posts/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+const posts = document.querySelector("#posts");
+ReactDOM.render(<App />, posts);

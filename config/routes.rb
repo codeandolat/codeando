61706af89c48840e8326c1a73ea4c7630e212eb1
David Sanchez <sanchez.dav90@gmail.com>
2018-11-04 23:45:57 -0600

    Re-order routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'home#index'
+
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
   post "/graphql", to: "graphql#execute"
-  devise_for :users
-  root to: 'home#index'
-  
+
   namespace :admin do
     get '/', to: 'home#index'
     resources :posts

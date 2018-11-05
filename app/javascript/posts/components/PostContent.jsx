5f99cf1347f9e37d1851521885f4b48071c2511b
David Sanchez <sanchez.dav90@gmail.com>
2018-11-04 20:40:50 -0600

    Add PostContent component

---
+++ app/javascript/posts/components/PostContent.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class PostContent extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="title is-3">
+          10 Interview Questions Every JavaScript Developer Should Know
+        </h1>
+        <p className="big-margin-bottom">
+          I was young, single, and freshly employed to direct National
+          Journal’s Presentation Center, a searchable library of
+          white-label PowerPoint presentations on political and policy
+          developments. By day, I led a team of fellows in creating data
+          visualizations...
+        </p>
+      </div>
+    )
+  }
+}
+
+export default PostContent

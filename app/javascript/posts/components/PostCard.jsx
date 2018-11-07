07af3f9aeac77425d0526999bb0884d41ddaee34
David Sanchez <sanchez.dav90@gmail.com>
2018-11-06 22:47:13 -0600

    Add PostProvider to PostCard

--- app/javascript/posts/components/PostCard.jsx
+++ app/javascript/posts/components/PostCard.jsx
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 
-import PostHeader from './PostHeader'
-import PostContent from './PostContent'
-import PostFooter from './PostFooter'
+import PostHeader from './PostHeader';
+import PostContent from './PostContent';
+import PostFooter from './PostFooter';
+
+import { PostProvider } from '../contexts/PostContext';
 
 class PostCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      post: this.props.post
+    };
+  }
+
   render() {
-    return(
-      <div className="card big-margin-bottom big-green-border-left rounded-borders">
-        <PostHeader />
-        <div className="card-content">
-          <PostContent />
-          <hr />
-          <PostFooter />
+    return (
+      <PostProvider value={{ post: this.state.post}}>
+        <div className="card big-margin-bottom big-green-border-left rounded-borders">
+          <PostHeader />
+          <div className="card-content">
+            <PostContent />
+            <hr />
+            <PostFooter />
+          </div>
         </div>
-      </div>
-    )
+      </PostProvider>
+    );
   }
 }
 

b01503d20a6cb3cb2c78fd482cdc060fe587b770
David Sanchez <sanchez.dav90@gmail.com>
2018-11-06 22:47:31 -0600

    Add PostConsumer to PostContent

--- app/javascript/posts/components/PostContent.jsx
+++ app/javascript/posts/components/PostContent.jsx
@@ -1,20 +1,22 @@
 import React, { Component } from 'react'
 
+import { PostConsumer } from "../contexts/PostContext";
+
 class PostContent extends Component {
   render() {
     return (
-      <div>
-        <h1 className="title is-3">
-          10 Interview Questions Every JavaScript Developer Should Know
-        </h1>
-        <p className="big-margin-bottom">
-          I was young, single, and freshly employed to direct National
-          Journal’s Presentation Center, a searchable library of
-          white-label PowerPoint presentations on political and policy
-          developments. By day, I led a team of fellows in creating data
-          visualizations...
-        </p>
-      </div>
+      <PostConsumer>
+        {({ post }) => (
+          <div>
+            <h1 className="title is-3">
+              {post.title}
+            </h1>
+            <p className="big-margin-bottom">
+              {post.subtitle}
+            </p>
+          </div>
+        )}
+      </PostConsumer>
     )
   }
 }

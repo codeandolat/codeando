d95fd87f95979c96d19a86c4d9511dae557a0878
David Sanchez <sanchez.dav90@gmail.com>
2018-11-06 22:44:42 -0600

    Add Apollo configuration

--- app/javascript/posts/index.js
+++ app/javascript/posts/index.js
@@ -1,6 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
+
 import App from "./components/App";
 
+const client = new ApolloClient({
+  // By default, this client will send queries to the
+  //  `/graphql` endpoint on the same host
+  // Pass the configuration option { uri: YOUR_GRAPHQL_API_URL } to the `HttpLink` to connect
+  // to a different host
+  link: new HttpLink(),
+  cache: new InMemoryCache()
+});
+
 const posts = document.querySelector("#posts");
-ReactDOM.render(<App />, posts);
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  posts
+);

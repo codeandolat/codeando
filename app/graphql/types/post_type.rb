5868dedb92a7b681e689d68a0257360866503dc6
David Sanchez <sanchez.dav90@gmail.com>
2018-11-07 23:19:57 -0600

    Add created_at to post_type

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -3,6 +3,7 @@ module Types
     field :id, ID, null: false
     field :title, String, null: false
     field :subtitle, String, null: false
+    field :created_at, String, null: false
     field :author, Types::UserType, null: false
   end
 end

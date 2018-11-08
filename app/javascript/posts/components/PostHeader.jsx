0167f8c749ef4b5cbe2a604cfa80dec3fcbd289d
David Sanchez <sanchez.dav90@gmail.com>
2018-11-07 23:20:53 -0600

    Consume createdAt in PostHeader component

--- app/javascript/posts/components/PostHeader.jsx
+++ app/javascript/posts/components/PostHeader.jsx
@@ -1,19 +1,28 @@
 import React, { Component } from 'react'
+import TimeAgo from 'react-timeago'
+
+import { PostConsumer } from '../contexts/PostContext'
 
 class PostHeader extends Component {
   render() {
     return(
-      <header className="card-header post-header">
-        <div className="columns">
-          <div className="column is-6">
-            <p className="card-header-title has-text-grey-light">12 hours ago</p>
-          </div>
-          <div className="column is-6 tags post-header-tags">
-            <span className="tag is-rounded">ruby</span>
-            <span className="tag is-rounded">javascript</span>
-          </div>
-        </div>
-      </header>
+      <PostConsumer>
+        {({ post }) => (
+          <header className="card-header post-header">
+            <div className="columns">
+              <div className="column is-6">
+                <p className="card-header-title has-text-grey-light">
+                  <TimeAgo date={post.createdAt} />
+                </p>
+              </div>
+              <div className="column is-6 tags post-header-tags">
+                <span className="tag is-rounded">ruby</span>
+                <span className="tag is-rounded">javascript</span>
+              </div>
+            </div>
+          </header>
+        )}
+      </PostConsumer>
     )
   }
 }

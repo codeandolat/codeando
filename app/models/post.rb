c2e2295152543590b4be95778ad35bfa4185f021
David Sanchez <sanchez.dav90@gmail.com>
2018-11-13 21:06:54 -0600

    Add friendly_id title in Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@ class Post < ApplicationRecord
   acts_as_taggable
   has_one_attached :image
 
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
   validates :title, :subtitle, presence: true
 
   belongs_to :author, class_name: 'User', foreign_key: 'user_id', counter_cache: true

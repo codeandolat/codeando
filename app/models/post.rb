d5aae003283a888528a03b694d3e9b2294862e95
David Sanchez <sanchez.dav90@gmail.com>
2018-11-13 21:19:38 -0600

    Add title la finder for Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,15 @@ class Post < ApplicationRecord
   has_one_attached :image
 
   extend FriendlyId
-  friendly_id :title, use: :slugged
+  friendly_id :title, :use => [:slugged, :finders]
 
   validates :title, :subtitle, presence: true
 
   belongs_to :author, class_name: 'User', foreign_key: 'user_id', counter_cache: true
+
+  private
+
+  def should_generate_new_friendly_id?
+    title_changed?
+  end
 end

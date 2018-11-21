6f7d1d5e2d478912048840abbb0caf8874d375a3
David Sanchez <sanchez.dav90@gmail.com>
2018-11-20 21:16:29 -0600

    Add Read more button to Post component

--- app/javascript/posts/components/PostFooter.jsx
+++ app/javascript/posts/components/PostFooter.jsx
@@ -1,23 +1,32 @@
 import React, { Component } from "react"
+import { Link } from 'react-router-dom'
+
+import { PostConsumer } from "../contexts/PostContext";
 
 class PostFooter extends Component {
   render() {
     return (
-      <div className="columns post-footer">
-        <div className="column is-6">
-          <button className="button is-rounded is-outlined is-success">Ver mas</button>
-        </div>
-        <div className="column is-6 post-footer-icons">
-          <div className="post-footer-item">
-            <i className="fa fa-comment-o is-size-4"></i>
-            <span className="count">7</span>
+      <PostConsumer>
+        {({ post }) => (
+          <div className="columns post-footer">
+            <div className="column is-6">
+              <Link to={`/posts/${post.slug}`} className="button is-rounded is-outlined is-success">
+                Read More
+              </Link>
+            </div>
+            <div className="column is-6 post-footer-icons">
+              <div className="post-footer-item">
+                <i className="fa fa-comment-o is-size-4"></i>
+                <span className="count">7</span>
+              </div>
+              <div className="post-footer-item">
+                <i className="fa fa-heart is-size-4"></i>
+                <span className="count">10</span>
+              </div>
+            </div>
           </div>
-          <div className="post-footer-item">
-            <i className="fa fa-heart is-size-4"></i>
-            <span className="count">10</span>
-          </div>
-        </div>
-      </div>
+        )}
+      </PostConsumer>
     )
   }
 }

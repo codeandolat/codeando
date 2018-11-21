f508d1fa80d9e7f3f6177997dcc3d6c5355784e1
David Sanchez <sanchez.dav90@gmail.com>
2018-11-20 21:16:05 -0600

    Add react router config

--- app/javascript/posts/components/App.jsx
+++ app/javascript/posts/components/App.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Posts from "./Posts";
+import Post from "./Post";
+import Notfound from "./NotFound";
 
 class App extends Component {
   render() {
-    return(
-      <div>
-        <Posts />
-      </div>
-    );
+    return <Router>
+        <Switch>
+          <Route exact path="/" component={Posts} />
+          <Route path="/posts/:slug" component={Post} />
+          <Route component={Notfound} />
+        </Switch>
+      </Router>;
   }
 }
 export default App

fac8fed942d82cbae5b47b44f70d07ac46721274
David Sanchez <sanchez.dav90@gmail.com>
2018-11-22 16:23:10 -0600

    Remove react-router implementation

--- app/javascript/posts/components/App.jsx
+++ app/javascript/posts/components/App.jsx
@@ -1,19 +1,12 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Posts from "./Posts";
-import Post from "./Post";
-import Notfound from "./NotFound";
 
 class App extends Component {
   render() {
-    return <Router>
-        <Switch>
-          <Route exact path="/" component={Posts} />
-          <Route path="/posts/:slug" component={Post} />
-          <Route component={Notfound} />
-        </Switch>
-      </Router>;
+    return(
+      <Posts />
+    )
   }
 }
 export default App

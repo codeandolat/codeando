c7c374d75ca8448b6c8f00e6777c851b7c99b09c
David Sanchez <sanchez.dav90@gmail.com>
2018-11-22 16:21:52 -0600

    Refactor graphql client

--- app/javascript/posts/index.js
+++ app/javascript/posts/index.js
@@ -1,20 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ApolloClient } from "apollo-client";
-import { HttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloProvider } from "react-apollo";
 
-import App from "./components/App";
+import { client } from '../shared/apollo-client'
 
-const client = new ApolloClient({
-  // By default, this client will send queries to the
-  //  `/graphql` endpoint on the same host
-  // Pass the configuration option { uri: YOUR_GRAPHQL_API_URL } to the `HttpLink` to connect
-  // to a different host
-  link: new HttpLink(),
-  cache: new InMemoryCache()
-});
+import App from "./components/App";
 
 const posts = document.querySelector("#posts");
 ReactDOM.render(

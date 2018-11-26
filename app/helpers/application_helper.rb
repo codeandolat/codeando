639531fa87ca42d7662c4fd27da95727811df1df
David Sanchez <sanchez.dav90@gmail.com>
2018-11-25 22:36:23 -0600

    Add state button helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,23 @@ module ApplicationHelper
     when :alert then "notification is-warning"
     end
   end
+
+  def state_button(post)
+    if @post.draft? || @post.rejected?
+      link_to 'Verify',
+              verify_admin_post_path(@post),
+              method: :post,
+              class: 'button is-success is-outlined'
+    elsif @post.verified?
+      link_to 'Publish',
+              publish_admin_post_path(@post),
+              method: :post,
+              class: 'button is-success'
+    elsif @post.published?
+      link_to 'Deprecate',
+              deprecate_admin_post_path(@post),
+              method: :post,
+              class: 'button is-danger is-outlined'
+    end
+  end
 end

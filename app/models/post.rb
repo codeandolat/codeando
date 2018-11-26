0feece94b59fd206ff913606b72d97c0afe252a8
David Sanchez <sanchez.dav90@gmail.com>
2018-11-25 22:35:18 -0600

    Add states and transitions to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   acts_as_taggable
   has_one_attached :image
 
+  include AASM
+
   extend FriendlyId
   friendly_id :title, :use => [:slugged, :finders]
 
@@ -10,8 +12,34 @@ class Post < ApplicationRecord
 
   belongs_to :author, class_name: 'User', foreign_key: 'user_id', counter_cache: true
 
+  scope :published, -> { where(state: 'published') }
+
+  aasm column: 'state' do
+    state :draft, initial: true
+    state :verified
+    state :published
+    state :rejected
+
+    event :verify do
+      transitions from: [:draft, :rejected], to: :verified
+    end
+
+    event :reject do
+      transitions from: :draft, to: :rejected
+    end
+
+    event :publish do
+      transitions from: :verified, to: :published
+    end
+
+    event :deprecate do
+      transitions from: :published, to: :draft
+    end
+  end
+
   private
 
+  # Change slug if post title change
   def should_generate_new_friendly_id?
     title_changed?
   end

b8de389e53f6a7cb325c451ddf015a49129bef9f
David Sanchez <sanchez.dav90@gmail.com>
2018-11-30 13:52:30 -0600

    Create Post component

--- app/javascript/post/components/Post.jsx
+++ app/javascript/post/components/Post.jsx
@@ -1,11 +1,44 @@
-import React, { Component } from 'react'
+import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 
-class Post extends Component {
-  render() {
-    return(
-      <h1 className="title is-1">{this.props.post.title}</h1>
-    )
+import Loading from './Loading'
+
+const slug = location.pathname.split("/posts/")[1]
+
+const GET_POST = gql`
+  {
+    post(id: "${slug}") {
+      id
+      title
+      subtitle
+      createdAt
+      tagList
+      slug
+      author {
+        email
+      }
+    }
   }
-}
+`;
+
+const Post = () => {
+  return (
+    <Query query={GET_POST}>
+      {({ loading, error, data: { post } }) => {
+        if (loading) {
+          return <Loading />;
+        }
+        if (error) return `Error: ${error.message}`;
+
+        return (
+          <div>
+            <h1 className="title is-1">{post.title}</h1>
+          </div>
+        );
+      }}
+    </Query>
+  )
+};
 
 export default Post

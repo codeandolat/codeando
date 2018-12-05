672adee58db0228b52bf4a58f7f74a3f16af52e9
David Sanchez <sanchez.dav90@gmail.com>
2018-12-04 21:27:04 -0600

    Add image_url to PostType

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -3,9 +3,15 @@ module Types
     field :id, ID, null: false
     field :title, String, null: false
     field :subtitle, String, null: false
+    field :image_url, String, null: false
     field :created_at, String, null: false
     field :slug, String, null: false
     field :tag_list, [String], null: true
     field :author, Types::UserType, null: false
+
+    def image_url
+      attachment = Post.find(id).image_attachment
+      Rails.application.routes.url_helpers.rails_blob_url(attachment, only_path: true)
+    end
   end
 end

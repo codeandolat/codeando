dfb9c08a1e612c79bf116465ff8a2737a221df9e
David Sanchez <sanchez.dav90@gmail.com>
2018-12-04 23:55:57 -0600

    Lint loaders css

--- app/javascript/post/components/Loading.jsx
+++ app/javascript/post/components/Loading.jsx
@@ -4,10 +4,10 @@ class Loading extends Component {
   render() {
     return(
       <div className="loader-container">
-        <div class="loader-balls">
-          <div class="loader-balls-item"></div>
-          <div class="loader-balls-item"></div>
-          <div class="loader-balls-item"></div>
+        <div className="loader-balls">
+          <div className="loader-balls-item"></div>
+          <div className="loader-balls-item"></div>
+          <div className="loader-balls-item"></div>
         </div>
       </div>
     )

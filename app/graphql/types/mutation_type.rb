731d2ee9faa875c286b8605d1f6d31def3601deb
David Sanchez <sanchez.dav90@gmail.com>
2019-01-07 14:50:38 -0600

    Mutation example

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,15 @@
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
+    field :create_post, Types::PostType, null: false do
+      argument :title, String, required: true
+      argument :subtitle, String, required: true
+    end
+
+    def create_post(title:, subtitle:)
+      context[:current_user].posts.create(
+        title: title,
+        subtitle: subtitle
+      )
     end
   end
 end

d2491526a72805ce49a42508e04dfe572055131b
David <sanchez.dav90@gmail.com>
2019-03-10 23:22:50 -0600

    Add posts type in Query type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,10 +3,19 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    field :posts, [Types::PostType], null: false,
-      description: "List all posts"
-    def posts
-      Post.all.last_published
+    field :posts, [Types::PostType], null: false do
+      argument :posts_type, String, required: true
+    end
+
+    def posts(posts_type:)
+      case posts_type
+      when 'all'
+        Post.last_published
+      when 'blog'
+        Post.without_videos
+      else
+        Post.all.last_published
+      end
     end
 
     field :post, Types::PostType, null: false do

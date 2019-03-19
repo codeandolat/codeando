ef871e9f6b502534bfbead14011817834043145c
David <sanchez.dav90@gmail.com>
2019-03-18 22:23:06 -0600

    Add pagination to Blog and Tutorials

--- app/javascript/tutorials/components/Posts.jsx
+++ app/javascript/tutorials/components/Posts.jsx
@@ -3,6 +3,7 @@ import { Query } from 'react-apollo';
 
 import PostCard from '../../shared/components/PostCard';
 import PostLoading from '../../shared/components/PostLoading';
+import LoadMore from "../../shared/components/LoadMore";
 import { GET_POSTS } from '../../shared/get_posts'
 
 const VARIABLES = {
@@ -12,7 +13,7 @@ const VARIABLES = {
 const Posts = () => {
   return (
     <Query query={GET_POSTS} variables={VARIABLES}>
-      {({ loading, error, data: { posts } }) => {
+      {({ loading, error, data: { postsConnection }, fetchMore }) => {
         if (loading) {
           return (
             <div>
@@ -20,20 +21,26 @@ const Posts = () => {
               <PostLoading />
               <PostLoading />
             </div>
-          )
+          );
         }
         if (error) return `Error: ${error.message}`;
 
         return (
           <React.Fragment>
-            {posts.map(post => (
-              <PostCard key={post.id} post={post} />
+            {postsConnection.edges.map(({ node }) => (
+              <PostCard key={node.id} post={node} />
             ))}
+            {postsConnection.pageInfo.hasNextPage && (
+              <LoadMore
+                postsConnection={postsConnection}
+                fetchMore={fetchMore}
+              />
+            )}
           </React.Fragment>
         );
       }}
     </Query>
-  )
+  );
 };
 
 export default Posts;

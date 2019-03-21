06548caf8e9e033d5f9f4594bae0896f166939ab
David <sanchez.dav90@gmail.com>
2019-03-20 22:05:11 -0600

    Set keep releases in deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,8 @@ set :deploy_to, '/home/deploy/codeando'
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
+set :keep_releases, 5
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 

6a4534247be18d809460644487a50c4305f5095e
David <sanchez.dav90@gmail.com>
2019-03-20 22:04:51 -0600

    Add last published scope to posts in admin

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -4,7 +4,7 @@ class Admin::PostsController < Admin::BaseController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.eager_load(:author, image_attachment: :blob)
+    @posts = Post.eager_loading.last_published
   end
 
   # GET /posts/1

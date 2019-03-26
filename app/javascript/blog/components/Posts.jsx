9cbd2a4b295295e20b64b75697a7f6d245b2a5a6
David <sanchez.dav90@gmail.com>
2019-03-25 22:42:10 -0600

    Add tags filter to blog view

--- app/javascript/blog/components/Posts.jsx
+++ app/javascript/blog/components/Posts.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
-import { Query } from 'react-apollo';
+import React from "react";
+import { Query } from "react-apollo";
+import queryString from "query-string";
 
-import PostCard from '../../shared/components/PostCard';
-import PostLoading from '../../shared/components/PostLoading';
+import PostCard from "../../shared/components/PostCard";
+import PostLoading from "../../shared/components/PostLoading";
 import LoadMore from "../../shared/components/LoadMore";
-import { GET_POSTS } from '../../shared/get_posts'
+import { GET_POSTS } from "../../shared/get_posts";
 
 const VARIABLES = {
   postsType: "blog"
+};
+
+const getFilteredPosts = (posts, tag) => {
+  return (
+    <React.Fragment>
+      {posts.edges
+        .filter(({ node }) => node.tagList.includes(tag))
+        .map(({ node }) => (
+          <PostCard key={node.id} post={node} />
+        ))}
+      {posts.pageInfo.hasNextPage && (
+        <LoadMore postsConnection={posts} fetchMore={fetchMore} />
+      )}
+    </React.Fragment>
+  );
+};
+
+const getPosts = (posts) => {
+  return (
+    <React.Fragment>
+      {posts.edges.map(({ node }) => (
+        <PostCard key={node.id} post={node} />
+      ))}
+      {posts.pageInfo.hasNextPage && (
+        <LoadMore postsConnection={posts} fetchMore={fetchMore} />
+      )}
+    </React.Fragment>
+  );
 };
 
 const Posts = () => {
@@ -25,19 +54,13 @@ const Posts = () => {
         }
         if (error) return `Error: ${error.message}`;
 
-        return (
-          <React.Fragment>
-            {postsConnection.edges.map(({ node }) => (
-              <PostCard key={node.id} post={node} />
-            ))}
-            {postsConnection.pageInfo.hasNextPage && (
-              <LoadMore
-                postsConnection={postsConnection}
-                fetchMore={fetchMore}
-              />
-            )}
-          </React.Fragment>
-        );
+        let queryTag = queryString.parse(location.search);
+
+        if (queryTag.tag) {
+          return getFilteredPosts(postsConnection, queryTag.tag);
+        } else {
+          return getPosts(postsConnection);
+        }
       }}
     </Query>
   );

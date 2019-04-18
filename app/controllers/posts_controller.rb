933c86184599eb4b2dcfae007ecc97d0a50cc954
David <sanchez.dav90@gmail.com>
2019-04-17 20:50:42 -0500

    Remove post show request

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,4 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
-
   def show
-  end
-
-  private
-
-  def set_post
-    @post = Post.find(params[:id])
   end
 end

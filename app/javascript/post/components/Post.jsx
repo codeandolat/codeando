1fd3c58d91100f75eec08d010895bc9651608a84
David <sanchez.dav90@gmail.com>
2019-04-17 21:27:01 -0500

    Add Helmet tags for facebook

--- app/javascript/post/components/Post.jsx
+++ app/javascript/post/components/Post.jsx
@@ -18,6 +18,13 @@ class Post extends Component {
     return(
       <PostProvider value={{ post: this.state.post }}>
         <Helmet>
+          <meta property="og:title" content={this.state.post.title} />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content={"https://code-ando.com/posts/" + this.state.post.slug} />
+          <meta property="og:image" content="https://code-ando.com/assets/avatar.png" />
+          <meta property="og:description" content={this.state.post.subtitle} />
+          <meta property="og:site_name" content={this.state.post.title} />
+
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:description" content={this.state.post.subtitle} />
           <meta name="twitter:title" content={this.state.post.title} />
